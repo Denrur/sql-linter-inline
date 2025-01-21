4083cc136bdad3cac506ce78ddce1114fe7712a3
mihsan <mihsan395@gmail.com>
2025-01-21 16:12:44 +0300

    bug fix

--- src/extension.ts
+++ src/extension.ts
@@ -272,7 +272,7 @@ export async function activate(context: vscode.ExtensionContext) {
       if (event.affectsConfiguration(`${PREFIX}.${key}`)) {
         let client: DatabaseConnect = getDB();
         const config = vscode.workspace.getConfiguration(PREFIX);
-        const dbPath = config.get<string>("dbPath");
+        let dbPath = config.get<string>("dbPath");
         let configObjFromConfig = { ...getConfig() };
         let configObjFromPath: Configuration = {};
         if (
@@ -298,6 +298,10 @@ export async function activate(context: vscode.ExtensionContext) {
             );
           } else {
             updateConfig({ ...configObjFromConfig, ...configObjFromPath });
+            dbPath = config.get<string>("dbPath");
+            if (dbPath) {
+              client.setConnectionString(dbPath);
+            }
           }
         } finally {
           isUpdatingConfig = false;
@@ -320,7 +324,6 @@ export async function activate(context: vscode.ExtensionContext) {
 
   await subscribeToDocumentChanges(context, inlinesqlDiagnostics, log);
   await vscode.commands.executeCommand(`${PREFIX}.updateTables`);
-
 }
 
 // This method is called when your extension is deactivated
